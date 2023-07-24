7a4034e7dfa0532dad4606857b76e7990f00a899
FaultyIce <m@aqu.me.uk>
2023-07-24 01:09:23 +0100

    see above

--- src/app/api/character/route.ts
+++ src/app/api/character/route.ts
@@ -1,12 +1,6 @@
 import { examplePlayer, examplePlayer2 } from "@/data/character/example";
 import { NextResponse } from "next/server";
-import { z } from "zod";
 
-export const characterGetRequest = z.object({
-  //   id: z.string().uuid(),
-  id: z.number(),
-});
-export type characterGetRequest = z.infer<typeof characterGetRequest>;
 
 const chars = [examplePlayer, examplePlayer2];
 export async function GET() {
